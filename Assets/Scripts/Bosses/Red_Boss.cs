d2850fd97bfad4dbe4647cfd5ca09b789772b311
JamesLaneProgramming <jamesjrlane@gmail.com>
2017-09-17 01:27:34 +1000

    [Added Boss Base Class, Red Boss Class]

--- Assets/Scripts/Bosses/Red_Boss.cs
+++ Assets/Scripts/Bosses/Red_Boss.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Red_Boss : Boss_Base_Class {
 
-    public float attack_Charge_Time;
+    public float attack_Charge_Delay;
+    [Range(0, 1)]
     public float attack_Charge_Force;
     private Rigidbody my_Rigidbody;
     private Vector3 targetDirection;
     private Vector3 target_Position;
+
+    private bool is_Stunned;
+    public float stun_Duration;
+
+    [Range(0, 0.1f)]
+    public float rotation_Speed;
 
     private void Awake()
     {
@@ -17,7 +24,8 @@ public class Red_Boss : Boss_Base_Class {
 
     public override void continue_Moving()
     {
-        transform.LookAt(player.transform);
+        Vector3 target_Rotation = Vector3.RotateTowards(transform.forward, (player.transform.position - transform.position).normalized, 1 * rotation_Speed, 1);
+        transform.rotation = Quaternion.LookRotation(target_Rotation);
         transform.position = Vector3.MoveTowards(transform.position, calculate_Target_Position(player), 1);
         base.continue_Moving();
     }
@@ -29,31 +37,44 @@ public class Red_Boss : Boss_Base_Class {
     }
     public override void continue_Attack()
     {
-        transform.LookAt(player.transform);
-        if ((Time.time - time_Targeted) > attack_Charge_Time)
+        //transform.LookAt(player.transform);
+        if ((Time.time - time_Targeted) > attack_Charge_Delay)
         {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + targetDirection, attack_Charge_Force);
         }
         base.continue_Attack();
     }
 
-    ///public override void start_Recover()
-    ///{
-    ///    target_Position = transform.position - targetDirection;
-    ///    base.start_Recover();
-    ///}
-    ///public override void continue_Recover()
-    ///{
-    ///    transform.position = Vector3.MoveTowards(transform.position, ///target_Position, 0.1f);
-    ///    if((transform.position - target_Position).magnitude <= 0.2f)
-    ///    {
-    ///        has_Recovered = true;
-    ///    }
-    ///    base.continue_Recover();
-    ///}
+    public override void start_Recover()
+    {
+        target_Position = transform.position - targetDirection;
+        base.start_Recover();
+        StartCoroutine(wait(stun_Duration, reset_Pos));
+    }
+    public override void continue_Recover()
+    {
+        if(is_Stunned == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target_Position, 0.1f);
+            if ((transform.position - target_Position).magnitude <= 0.2f)
+            {
+                has_Recovered = true;
+            }
+        }
+        base.continue_Recover();
+    }
 
     public void OnCollisionEnter(Collision collided_Object)
     {
+        if(collided_Object.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        {
+            is_Stunned = true;
+        }
         is_Attacking = false;
+    }
+
+    public void reset_Pos()
+    {
+        is_Stunned = false;
     }
 }
